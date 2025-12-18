19a809017be3336eab99d754cb4950225c0a8339
Magnús Magnússon <magnus@mmagnusson.net>
2025-12-18 02:48:08 +0000

    revert blocks until ready

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "DEBUG_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG_ENABLED", False) == "True"
+DEBUG = os.environ.get("DJANGO_DEBUG_ENABLED", "True") == "True"
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
 
